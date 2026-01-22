4a7ac187a3440f460ca8b4a2034284bbea848505
4M <joon7575@gmail.com>
2026-01-22 17:16:35 +0900

    Fix future question bugs and pass all tests

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -7,10 +8,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-    # def was_published_recently(self):
-    #     from django.utils import timezone
-    #     import datetime
-    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        from django.utils import timezone
+        import datetime
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
--- polls/views.py
+++ polls/views.py
@@ -1,33 +1,95 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
-from .models import Choice
+from django.db.models import F
+from django.urls import reverse
+from django.views import generic
+from django.http import HttpResponseRedirect
+from .models import Question, Choice
+from django.utils import timezone
+
+# 메인 페이지 (질문 목록)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+
+    def get_queryset(self):
+        return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by("-pub_date")[:5]
+        # return Question.objects.order_by("-pub_date")[:5]
+
+
+# 질문 상세 페이지
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    context_object_name = "question"
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+# 결과 페이지
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+    context_object_name = "question"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+# 투표 처리 로직
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message":"You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+# from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse
+# from .models import Question
+# from .models import Choice
 
 # index(최신글 list)
-def index(request):
-	# return HttpResponse("Hello) 기존코드
+# def index(request):
+# 	# return HttpResponse("Hello) 기존코드
 	
-	latest_question_list = Question.objects.order_by("-pub_date")[:5]
-	context = {"latest_question_list": latest_question_list}
-	return render(request, "polls/index.html", context)
+# 	latest_question_list = Question.objects.order_by("-pub_date")[:5]
+# 	context = {"latest_question_list": latest_question_list}
+# 	return render(request, "polls/index.html", context)
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, "polls/detail.html", {"question": question})
+# def detail(request, question_id):
+# 	question = get_object_or_404(Question, pk=question_id)
+# 	return render(request, "polls/detail.html", {"question": question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
 
-def vote(request, question_id):
-      return HttpResponse(f"You're voting on question {question_id}.")
+# def vote(request, question_id):
+#       return HttpResponse(f"You're voting on question {question_id}.")
 
-def aa(request):
-       data = Question.objects.all()
-       data_c = Choice.objects.all()
-       context = {"questions":data,
-                  "choices":data_c}
-       return render(request, "polls/aa.html",context)
+# def aa(request):
+#        data = Question.objects.all()
+#        data_c = Choice.objects.all()
+#        context = {"questions":data,
+#                   "choices":data_c}
+#        return render(request, "polls/aa.html",context)
+
+
 
 
 # # Create your views here.
